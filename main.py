bb0758f08d71c31d4c52b31b113943cd4fead40a
kebasyaty <kebasyaty@gmail.com>
2025-11-25 15:38:40 +0200

    add main files

---
+++ main.py
@@ -0,0 +1,12 @@
+"""???"""
+
+from __future__ import annotations
+
+
+def main() -> None:
+    """???"""
+    print("Hello from defrag!")  # noqa: T201
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_stub.py
@@ -0,0 +1,19 @@
+"""Test Stub."""
+
+from __future__ import annotations
+
+
+class TestNegative:
+    """Negative tests."""
+
+    async def test_stub(self) -> None:
+        """Testing stub."""
+        assert True
+
+
+class TestPositive:
+    """Positive tests."""
+
+    async def test_stub(self) -> None:
+        """Testing stub."""
+        assert True
