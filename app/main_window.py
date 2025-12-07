e67d432f46d02e29924a48a12e89fc70de5a1797
kebasyaty <kebasyaty@gmail.com>
2025-12-07 07:42:18 +0200

    update MainWindow

--- app/main_window.py
+++ app/main_window.py
@@ -12,26 +12,9 @@ class MainWindow(Gtk.ApplicationWindow):
     def __init__(self, **kwargs) -> None:  # type: ignore[no-untyped-def]  # noqa: D107
         super().__init__(**kwargs)
 
-        # Create a vertical box
-        vbox = Gtk.Box.new(Gtk.Orientation.VERTICAL, 6)  # Spacing of 6 pixels
-        vbox.set_margin_top(12)
-        vbox.set_margin_start(6)
-        vbox.set_margin_end(6)
-        vbox.set_margin_bottom(6)
-        self.set_child(vbox)  # Set the box as the main child of the window
-
         # Create a horizontal box
         hbox = Gtk.Box.new(Gtk.Orientation.HORIZONTAL, 6)
-        vbox.append(hbox)  # Add the horizontal box to the vertical box
-
-        # Create some buttons
-        button1 = Gtk.Button.new_with_label("Button 1")
-        button2 = Gtk.Button.new_with_label("Button 2")
-        button3 = Gtk.Button.new_with_label("Button 3")
-
-        # Add buttons to the horizontal box
-        hbox.append(button1)
-        hbox.append(button2)
-
-        # Add another button directly to the vertical box
-        vbox.append(button3)
+        hbox.set_margin_top(12)
+        hbox.set_margin_start(6)
+        hbox.set_margin_end(6)
+        hbox.set_margin_bottom(6)
