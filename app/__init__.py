e425d4f1d0f9877eb12f57a66c835ec4eee04866
kebasyaty <kebasyaty@gmail.com>
2025-12-08 17:36:35 +0200

    update

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,16 @@ from __future__ import annotations
 
 __all__ = ("Defrag",)
 
+import gi
 
-from gi.repository import GLib, Gtk  # pyright: ignore[reportMissingModuleSource]
+gi.require_version("Gtk", "4.0")
 
-from app.constants import APP_ID, APP_NAME
-from app.main_window import MainWindow
+
+import darkdetect  # noqa: E402
+from gi.repository import GLib, Gtk  # pyright: ignore[reportMissingModuleSource]  # noqa: E402
+
+from app.constants import APP_ID, APP_NAME  # noqa: E402
+from app.main_window import MainWindow  # noqa: E402
 
 
 class Defrag(Gtk.Application):
@@ -20,6 +25,13 @@ class Defrag(Gtk.Application):
 
     def do_activate(self) -> None:
         """Do activate."""
+        # Replace application theme on system theme
+        settings = Gtk.Settings.get_default()
+        if settings is not None:
+            settings.set_property(
+                "gtk-application-prefer-dark-theme",
+                darkdetect.isDark(),
+            )
         # Create an instance of the MainWindow class
         window = self.props.active_window
         if not window:
--- main.py
+++ main.py
@@ -2,26 +2,13 @@
 
 from __future__ import annotations
 
-import gi
+import sys
 
-gi.require_version("Gtk", "4.0")
-
-import sys  # noqa: E402
-
-import darkdetect  # noqa: E402
-from app import Defrag  # noqa: E402
-from gi.repository import Gtk  # pyright: ignore[reportMissingModuleSource] # noqa: E402
+from app import Defrag
 
 
 def main() -> None:
     """Business logic."""
-    # Replace application theme on system theme
-    settings = Gtk.Settings.get_default()
-    if settings is not None:
-        settings.set_property(
-            "gtk-application-prefer-dark-theme",
-            darkdetect.isDark(),
-        )
     # Create an instance of the Defrag class
     app = Defrag()
     # Run application
