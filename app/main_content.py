15930d741e159bd0d953b0138316af625f2f413b
kebasyaty <kebasyaty@gmail.com>
2025-12-14 19:44:45 +0200

    update

--- app/main_content.py
+++ app/main_content.py
@@ -4,7 +4,9 @@ from __future__ import annotations
 
 __all__ = ("MainContent",)
 
-from gi.repository import Gtk  # pyright: ignore[reportMissingModuleSource]
+from typing import Any
+
+from gi.repository import Gio, Gtk  # pyright: ignore[reportMissingModuleSource]
 
 
 class MainContent:
@@ -22,7 +24,47 @@ class MainContent:
         # Observe the children of `dynamic_page_vbox`
         children_model = self.dynamic_page_vbox.observe_children()
         # Iterate through the children of `dynamic_page_vbox`
-        for i in range(children_model.get_n_items()):
-            child = children_model.get_item(i)
+        for idx in range(children_model.get_n_items()):
+            child = children_model.get_item(idx)
             if isinstance(child, Gtk.Widget):
                 self.dynamic_page_vbox.remove(child)
+
+    def on_process_exit(self, process: Gio.Subprocess, res: Any) -> None:
+        """???"""
+        # Read the output streams in the callback
+        stdout_stream, stderr_stream = process.get_stdout_pipe(), process.get_stderr_pipe()
+
+        # Note: Reading streams is also typically done asynchronously in a real app,
+        # but for simplicity we use a blocking read here, which is fine since the process already finished
+        stdout_str = "???"
+        if stdout_stream is not None:
+            stdout_bytes = stdout_stream.read_bytes(1024, None)
+            tmp = stdout_bytes.get_data()
+            if tmp is not None:
+                stdout_str = tmp.decode("utf-8")
+
+        stderr_str = "???"
+        if stderr_stream is not None:
+            stderr_bytes = stderr_stream.read_bytes(1024, None)
+            tmp = stderr_bytes.get_data()
+            if tmp is not None:
+                stderr_str = tmp.decode("utf-8")
+
+        exit_code = process.get_exit_status()
+        if exit_code == 0:
+            print(f"Async Command Output (Exit Code {exit_code}):\n{stdout_str}")  # noqa: T201
+        else:
+            print(f"Async Command Failed (Exit Code {exit_code}):\n{stderr_str}")  # noqa: T201
+
+    def on_process_run(self, widget: Any, command_args: list[str]) -> None:
+        """???"""
+        # Create a GSubprocess
+        # 'flags' are important for proper I/O handling
+        process = Gio.Subprocess.new(
+            command_args,
+            Gio.SubprocessFlags.STDOUT_PIPE | Gio.SubprocessFlags.STDERR_PIPE,
+        )
+
+        # Asynchronously watch for the process termination
+        # When it exits, the callback function will be triggered
+        process.wait_async(None, self.on_process_exit)
--- app/sidebar.py
+++ app/sidebar.py
@@ -63,27 +63,50 @@ class Sidebar:
 
     def on_btn_health(self, widget: Any) -> None:
         """Handler for a Health button."""
-        self.clean_dynamic_page()
-        self.set_title_to_dynamic_page("Checking the integrity of HDD|SSD")
+        self.add_content_to_dynamic_page(
+            title_page="Checking the integrity of HDD|SSD",
+            btn_name="Run check health",
+            command_args=["ls", "-l"],
+        )
 
     def on_btn_cleaning(self, widget: Any) -> None:
         """Handler for a Cleaning button."""
-        self.clean_dynamic_page()
-        self.set_title_to_dynamic_page("Cleaning")
+        self.add_content_to_dynamic_page(
+            title_page="Cleaning",
+            btn_name="Run cleaning",
+            command_args=["ls", "-l"],
+        )
 
     def on_btn_analysis(self, widget: Any) -> None:
         """Handler for a Analysis button."""
-        self.clean_dynamic_page()
-        self.set_title_to_dynamic_page("Analysis file fragmentation")
+        self.add_content_to_dynamic_page(
+            title_page="Analysis file fragmentation",
+            btn_name="Run analysis",
+            command_args=["ls", "-l"],
+        )
 
     def on_btn_defrag(self, widget: Any) -> None:
         """Handler for a Defrag button."""
-        self.clean_dynamic_page()
-        self.set_title_to_dynamic_page("Defragmentation")
+        self.add_content_to_dynamic_page(
+            title_page="Defragmentation",
+            btn_name="Run defrag",
+            command_args=["ls", "-l"],
+        )
 
-    def set_title_to_dynamic_page(self, title: str) -> None:
-        """Add Title to `content_page_vbox`."""
+    def add_content_to_dynamic_page(
+        self,
+        title_page: str,
+        btn_name: str,
+        command_args: list[str],
+    ) -> None:
+        """Add content to dynamic page."""
+        self.clean_dynamic_page()
+        # Add Title page
         title_label = Gtk.Label()
-        title_label.set_markup(f"<b>{title}</b>")
+        title_label.set_markup(f"<b>{title_page}</b>")
         title_label.set_halign(Gtk.Align.START)
         self.dynamic_page_vbox.append(title_label)
+        # Add button run
+        btn_run = Gtk.Button(label=btn_name)
+        btn_run.connect("clicked", self.on_process_run, command_args)
+        self.dynamic_page_vbox.append(btn_run)
