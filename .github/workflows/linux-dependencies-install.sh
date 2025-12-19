08ed249de3be18d9304a8a79d9d963f707858ca7
kebasyaty <kebasyaty@gmail.com>
2025-12-19 20:49:47 +0200

    update linux-dependencies-install.sh

--- .github/workflows/linux-dependencies-install.sh
+++ .github/workflows/linux-dependencies-install.sh
@@ -9,5 +9,3 @@ apt install -y curl gnupg2 systemctl git
 apt install -y libgirepository-2.0-dev gcc libcairo2-dev pkg-config python3-dev gir1.2-gtk-4.0
 # Install UV
 curl -LsSf https://astral.sh/uv/install.sh | sh
-# Install dependencies with uv
-uv sync --locked --dev --group lint --group test
