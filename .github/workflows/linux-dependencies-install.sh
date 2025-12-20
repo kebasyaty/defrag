c06ab09ea505136d29e22af7b03ffe9e6a1faaaf
kebasyaty <kebasyaty@gmail.com>
2025-12-20 09:52:07 +0200

    update

--- .github/workflows/linux-dependencies-install.sh
+++ .github/workflows/linux-dependencies-install.sh
@@ -4,7 +4,7 @@ set -e
 
 apt update
 
-apt install sudo
+apt install -y sudo
 
 # Install sys tools
 sudo apt install -y libgirepository-2.0-dev gcc libcairo2-dev python3-cairo-dev pkg-config python3-dev gir1.2-gtk-4.0
