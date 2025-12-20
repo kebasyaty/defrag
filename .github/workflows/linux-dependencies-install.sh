b83909103ecda02b82091d5f4cb0ea3e95eee04e
kebasyaty <kebasyaty@gmail.com>
2025-12-20 09:34:28 +0200

    update linux-dependencies-install.sh

--- .github/workflows/linux-dependencies-install.sh
+++ .github/workflows/linux-dependencies-install.sh
@@ -5,4 +5,4 @@ set -e
 apt update
 
 # Install OS dependencies
-apt install -y libgirepository-2.0-dev gcc libcairo2-dev pkg-config python3-dev gir1.2-gtk-4.0
+apt install -y libgirepository-2.0-dev gcc libcairo2-dev python3-cairo-dev pkg-config python3-dev gir1.2-gtk-4.0
