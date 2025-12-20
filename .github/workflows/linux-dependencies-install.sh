2d536598a5c98874b2da6defd1b611c98601aeb7
kebasyaty <kebasyaty@gmail.com>
2025-12-20 11:03:34 +0200

    fix

--- .github/workflows/linux-dependencies-install.sh
+++ .github/workflows/linux-dependencies-install.sh
@@ -6,5 +6,3 @@ apt update
 
 # Install sys tools
 apt install -y libgirepository-2.0-dev gcc libcairo2-dev pkg-config python3-dev gir1.2-gtk-4.0
-# Installing the system provided PyGObject
-apt install -y python3-gi python3-gi-cairo gir1.2-gtk-4.0
