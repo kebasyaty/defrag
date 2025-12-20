5dc68022da0112b77fb58281f2dc67c6194ef114
kebasyaty <kebasyaty@gmail.com>
2025-12-20 10:18:21 +0200

    fix

--- .github/workflows/linux-dependencies-install.sh
+++ .github/workflows/linux-dependencies-install.sh
@@ -9,4 +9,4 @@ apt install -y sudo
 # Install sys tools
 sudo apt install -y libgirepository-2.0-dev gcc libcairo2-dev python3-cairo-dev pkg-config python3-dev python3-setuptools libevent-dev build-essential gir1.2-gtk-4.0
 
-pip install pycairo
+python3 -m pip install pycairo
