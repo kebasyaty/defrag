dad899f267b811aa068aafe7382903895ca09522
kebasyaty <kebasyaty@gmail.com>
2026-01-02 13:03:20 +0200

    fix config

--- defrag/config.py
+++ defrag/config.py
@@ -9,7 +9,7 @@ List of variables:
 - `APP_NAME` - Application name.
 - `APP_ID` - Application ID.
 - `WINDOW_DEFAULT_WIDTH` - Default application window width.
-- `WINDOW_DEFAULT_HEIGHT` - WINDOW_DEFAULT_HEIGHT.
+- `WINDOW_DEFAULT_HEIGHT` - Default application window height.
 """
 
 from __future__ import annotations
@@ -17,6 +17,8 @@ from __future__ import annotations
 __all__ = (
     "APP_NAME",
     "APP_ID",
+    "WINDOW_DEFAULT_WIDTH",
+    "WINDOW_DEFAULT_HEIGHT",
 )
 
 
